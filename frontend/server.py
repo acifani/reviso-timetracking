4a939340aefe53815db0b489a90353b412807aca
Alessandro <alessandro.cifani@gmail.com>
2017-07-08 13:18:07 +0200

    update frontend server now serves directly files into public directory to avoid having to run npm build to reflect changes

--- frontend/server.py
+++ frontend/server.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, current_app
 
 # set the project root directory as the static folder, you can set others.
-app = Flask(__name__, static_url_path='', static_folder='build')
+app = Flask(__name__, static_url_path='', static_folder='public')
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return render_template('index.html')
+    return current_app.send_static_file('index.html')
 
 if __name__ == "__main__":
     app.run()
