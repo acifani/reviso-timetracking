206a943f4dfc5eec73deb12ecd821225afeb2b0d
Alessandro <alessandro.cifani@gmail.com>
2017-07-16 20:22:04 +0200

    switch to api deployed on heroku

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://212.237.1.146:8889/api/v0.1/';
+// const BASE_URL = 'http://212.237.1.146:8889/api/v0.1/';
+const BASE_URL = 'http://reviso-timetracking-server.herokuapp.com/api/v0.1/';
 
 export const getAllEntries = () => ({
   type: 'FETCH_ENTRIES',
